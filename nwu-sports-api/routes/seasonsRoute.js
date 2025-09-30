98b58dfccd9bd92de02da677e1d0c25496c8e3d7
Oratile Kgatlhanye <103864481+Orah-23@users.noreply.github.com>
2025-09-30 19:45:42 +0200

    Delete nwu-sports-api directory

--- nwu-sports-api/routes/seasonsRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getSeasons } from '../controllers/seasonsController.js';
-
-const router = express.Router();
-
-router.get('/', getSeasons);
-
-export default router;
