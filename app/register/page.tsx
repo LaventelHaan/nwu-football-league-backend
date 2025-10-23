504cb0887ddb53db9438133ab03a7f7b520e0f73
Forget <104951117+09JESUS@users.noreply.github.com>
2025-10-23 13:24:04 +0200

    Fix case of 'Scout' in select options

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -237,7 +237,7 @@ export default function RegisterPage() {
                   <SelectContent>
                     <SelectItem value="player">Player</SelectItem>
                     <SelectItem value="coach">Coach</SelectItem>
-                    <SelectItem value="scout">Scout</SelectItem>
+                    <SelectItem value="Scout">Scout</SelectItem>
                   </SelectContent>
                 </Select>
               </div>
